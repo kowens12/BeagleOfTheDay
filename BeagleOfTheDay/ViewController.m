d40e423e67b79012244f2169a2b156ab863702f3
Katherine Owens <katherineowens@Katherines-MacBook-Pro.local>
2017-04-27 15:50:33 -0400

    Initial Commit

---
+++ BeagleOfTheDay/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BeagleOfTheDay
+//
+//  Created by Katherine Owens on 4/27/17.
+//  Copyright © 2017 Katherine Owens. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
