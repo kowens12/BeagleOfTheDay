e818c43ab7f791c7c0d9446bc8385a897f43f8d9
Katherine Owens <katherineowens@Katherines-MacBook-Pro.local>
2017-04-28 17:40:59 -0400

    KO - added NSURL and NSURL session to make test API call

--- BeagleOfTheDay/ViewController.m
+++ BeagleOfTheDay/ViewController.m
@@ -16,13 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self makeCall];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)makeCall {
+    
+    NSURLSession *URLSession = [NSURLSession sharedSession];
+    
+    NSURL *beagleURL = [NSURL URLWithString:@"http://www.dankota.com/calculate/01"];
+    
+    NSURLSessionDataTask *dataTask = [URLSession dataTaskWithURL:beagleURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error == nil) {
+            NSDictionary *dictionaryFromJSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            NSLog(@"");
+            NSString *responseString = [dictionaryFromJSON valueForKey:@"result"];
+            
+            NSLog(@"The response is %@", (responseString));
+        }
+    }];
+    
+    [dataTask resume];
 }
 
 
